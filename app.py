6d35f3955506f2106065de98340a0bdd8abd2093
Niyaazkhan300 <theultimategamercontrol@gmail.com>
2025-12-24 11:26:18 +0200

    Final Railway fix: all files in root, imports resolved

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from data import config
 from loader import dp, db, bot
 import filters
 
+# Setup filters
 filters.setup(dp)
 
 WEBAPP_HOST = "0.0.0.0"  # Host for webhook
@@ -77,8 +78,9 @@ if __name__ == '__main__':
     executor.start_webhook(
         dispatcher=dp,
         webhook_path=config.WEBHOOK_PATH,
-  on_startup=on_startup,
-  on_shutdown=on_shutdown,
-   skip_updates=True,
-   host=WEBAPP_HOST,
-   port=WEBAPP_PORT, )
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT
+    )
